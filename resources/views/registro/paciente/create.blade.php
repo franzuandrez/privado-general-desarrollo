f9fd7de766115f2075eb427728beeffa75580bd7
franzua <edson.andrez@outlook.es>
2022-12-05 17:18:48 -0600

    Boton de paciente

--- resources/views/registro/paciente/create.blade.php
+++ resources/views/registro/paciente/create.blade.php
@@ -1,75 +1,72 @@
 @extends('layout.app')
 @section('content')
-    @component('partitials.nav',['operation'=>'Nuevo',
-      'menu_icon'=>'mdi mdi-clipboard-text-outline link-icon',
-      'submenu_icon'=>'fa-solid fa-id-card icono',
-      'operation_icon'=>'fa-solid fa-plus',])
-        @slot('menu')
-            Registro
-        @endslot
-        @slot('submenu')
-            Paciente
-        @endslot
-    @endcomponent
+
 
     @include('partitials.messages')
 
-    <div class="col-lg-12">
-        <div class="grid">
-            <p class="grid-header">Registro</p>
-            <div class="grid-body">
-                <form action="{{ route('paciente.store') }}" method="POST">
-                    @csrf
+    <div class="row">
+        <div class="col col-md-12">
+            <div class="grid">
+                <p class="grid-header">Registar Paciente
+                </p>
 
-                    <div class="item-wrapper">
-                        <div class="row mb-3">
-                            <div class="col-md-8 mx-auto">
-                                <div class="form-group row showcase_row_area">
-                                    <div class="col-md-3 showcase_text_area">
-                                        <label for="inputType1">Nombres</label>
-                                    </div>
-                                    <div class="col-md-9 showcase_content_area">
-                                        <input type="text" name="nombres" class="form-control" value="{{ old('nombres') }}" autofocus>
+                    <div class="row">
+                        <div class="col col-md-6 col-xl-12">
+                            <form action="{{ route('paciente.store') }}" method="POST">
+                                @csrf
+
+                                <div class="item-wrapper">
+                                    <div class="row mb-3">
+                                        <div class="col-md-8 mx-auto">
+                                            <div class="form-group row showcase_row_area">
+                                                <div class="col-md-3 showcase_text_area">
+                                                    <label for="inputType1">Nombres</label>
+                                                </div>
+                                                <div class="col-md-9 showcase_content_area">
+                                                    <input type="text" name="nombres" class="form-control"
+                                                           value="{{ old('nombres') }}" autofocus>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row showcase_row_area">
+                                                <div class="col-md-3 showcase_text_area">
+                                                    <label for="inputType12">Apellidos</label>
+                                                </div>
+                                                <div class="col-md-9 showcase_content_area">
+                                                    <input type="text" name="apellidos" class="form-control"
+                                                           value="{{ old('apellidos') }}">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row showcase_row_area">
+                                                <div class="col-md-3 showcase_text_area">
+                                                    <label for="inputType13">Fecha de nacimiento</label>
+                                                </div>
+                                                <div class="col-md-9 showcase_content_area">
+                                                    <input type="text" name="fecha_nacimiento"
+                                                           class="form-control datepicker"
+                                                           value="{{ old('fecha_nacimiento') }}">
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="form-group row showcase_row_area">
-                                    <div class="col-md-3 showcase_text_area">
-                                        <label for="inputType12">Apellidos</label>
-                                    </div>
-                                    <div class="col-md-9 showcase_content_area">
-                                        <input type="text" name="apellidos" class="form-control" value="{{ old('apellidos') }}">
-                                    </div>
-                                </div>
-                                <div class="form-group row showcase_row_area">
-                                    <div class="col-md-3 showcase_text_area">
-                                        <label for="inputType13">Fecha de nacimiento</label>
-                                    </div>
-                                    <div class="col-md-9 showcase_content_area">
-                                        <input type="text" name="fecha_nacimiento" class="form-control datepicker"
-                                               value="{{ old('fecha_nacimiento') }}">
+                                <hr>
+                                <div class="row" style="text-align:center">
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-primary">Guardar</button>
+                                        <a class="btn btn-dark" href="{{url('/customers')}}" role="button">
+                                            Regresar
+                                        </a>
+
                                     </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
-                    <hr>
-                    <div class="row" style="text-align:center">
-                        <div class="col-12">
-                            <a href="{{ route('paciente.index') }}" class="btn btn-success">
-                                <i class="fa-solid fa-ban icono"></i>&nbsp;&nbsp;
-                                Cancelar
-                            </a>
 
-                            <button class="btn btn-primary" type="submit">
-                                <i class="fa-regular fa-floppy-disk icono"></i>&nbsp;&nbsp;Guardar
-                            </button>
-
-                        </div>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
+
 
 @endsection
 @section('scripts')
